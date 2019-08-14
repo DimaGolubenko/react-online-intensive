be61f5620118ef8339adc0375df708b7eaa8e65d
Dima Golubenko <dimagolubenko@gmail.com>
2019-08-14 19:08:44 +0300

    feat(source): finish react-online-lesson-2

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,17 +1,22 @@
 //Core
 import React, { Component } from 'react';
 
+//Instruments
+import Styles from './styles.m.css';
+
 //Components
+import StatusBar from '../StatusBar';
 import Composer from '../Composer';
 import Post from '../Post';
 
 export default class Feed extends Component {
     render() {
         return (
-            <>
+            <section className = { Styles.feed }>
+                <StatusBar />
                 <Composer />
                 <Post />
-            </>
+            </section>
         );
     }
 }

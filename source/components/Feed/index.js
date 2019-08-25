e92c00dd039bfec4ac00f0a169ea354b1f9cf0be
Dima Golubenko <dimagolubenko@gmail.com>
2019-08-25 15:30:29 +0300

    feat(source): finish react-online-lesson-5

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -5,16 +5,37 @@ import React, { Component } from 'react';
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
+import Spinner from 'components/Spinner';
 
 //Instruments
 import Styles from './styles.m.css';
 export default class Feed extends Component {
+    state = {
+        posts: [
+            { id: '123', comment: 'Hi there!', created: 1526825076849 },
+            { id: '456', comment: 'Приветик!', created: 1526825076855 },
+        ],
+        isPostsFetching: false,
+    };
+
     render() {
+        const { posts, isPostsFetching } = this.state;
+
+        const postsJSX = posts.map((post) => {
+            return (
+                <Post
+                    key = { post.id }
+                    { ...post }
+                />
+            );
+        });
+
         return (
             <section className = { Styles.feed }>
+                <Spinner isSpinning = { isPostsFetching } />
                 <StatusBar />
                 <Composer />
-                <Post />
+                {postsJSX}
             </section>
         );
     }
